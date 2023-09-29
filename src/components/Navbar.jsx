aaa5d5c783df0d09ce2bea0a9c4421d32fac26f0
omkarb81 <117004362+omkarb81@users.noreply.github.com>
2023-09-29 19:57:28 +0530

    changes in table and add images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
         </div>
         <div class="social-icon">
             <a target='_blank' href="https://www.linkedin.com/company/gdsc-dmce/"><i class="fa-brands fa-linkedin  linkedin" id="linkedin"></i></a>
-            <a target='_blank' href="https://github.com/Google-Developer-Students-Club-DMCE"><i class="fa-brands fa-github  linkedin" id="linkedin1"></i></a>
-            <a target='_blank' href="https://www.instagram.com/gdsc.dmce"><i class="fa-brands fa-instagram  linkedin" id="linkedin2"></i></a>
-            <a target='_blank' href="https://www.youtube.com/@gdsc_dmce"><i class="fa-brands fa-youtube linkedin"></i></a>
+            <a target='_blank' href="https://github.com/Google-Developer-Students-Club-DMCE"><i class="fa-brands fa-github  linkedin" id="github"></i></a>
+            <a target='_blank' href="https://www.instagram.com/gdsc.dmce"><i class="fa-brands fa-instagram  linkedin" id="instagram"></i></a>
+            <a target='_blank' href="https://www.youtube.com/@gdsc_dmce"><i class="fa-brands fa-youtube linkedin" id='youtube'></i></a>
         </div>
       </nav>
       
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,59 +1,20 @@
-import React from 'react';
-import "./Table.css";
-import { useState } from 'react';
+import React, { useState } from 'react';
+import './Table.css';
+import image1 from './images/image1.png';
+import image2 from './images/image2.png';
+import image3 from './images/image3.png';
+import image4 from './images/image4.png';
 
 const Table = () => {
+  const dummyData = [
+    {id: 1, NAME: 'Manas Kulkarni', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'not completed'},
+     {id: 2, NAME: 'Sandesh Jadhav', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'completed '},
+     {id: 3, NAME: 'Viraj Sharma', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'completed'},
+    {id: 4, NAME: 'Rohit Koli', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'not completed'},
+     {id: 5, NAME: 'Akkash Chopra', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'not completed'},
+   ]
 
-    const dummyData = [
-        {
-          id: 1,
-          NAME: 'Manas Kulkarni',
-          COURSES: 4,
-          SKILL: 4,
-          GEN_AI: 1,
-          DUE_DATE: '18/10/23',
-          STATUS: 'not completed',
-        },
-        {
-          id: 2,
-          NAME: 'Sandesh Jadhav',
-          COURSES: 4,
-          SKILL: 4,
-          GEN_AI: 1,
-          DUE_DATE: '18/10/23',
-          STATUS: 'completed',
-        },
-        {
-          id: 3,
-          NAME: 'Viraj Sharma',
-          COURSES: 4,
-          SKILL: 4,
-          GEN_AI: 1,
-          DUE_DATE: '18/10/23',
-          STATUS: 'completed',
-        },
-        {
-          id: 4,
-          NAME: 'Rohit Koli',
-          COURSES: 4,
-          SKILL: 4,
-          GEN_AI: 1,
-          DUE_DATE: '18/10/23',
-          STATUS: 'not completed',
-        },
-        {
-          id: 5,
-          NAME: 'Akkash Chopra',
-          COURSES: 4,
-          SKILL: 4,
-          GEN_AI: 1,
-          DUE_DATE: '18/10/23',
-          STATUS: 'not completed',
-        },
-      ];
-      
-
-       const [value,setValue] = useState('');
+   const [value,setValue] = useState('');
        const [dataSource,setDataSource] = useState(dummyData);
        const [tableFilter,setTableFilter] = useState([]);
        const filterData = (e) => {
@@ -68,68 +29,83 @@ const Table = () => {
             setDataSource([...dataSource])
         }
        }
-    
 
   return (
-    <div>
-        <div class="tableinfo">
-            <div class="Texthead"><h1>Google Cloud Study Jams 23-24</h1> <p class="Para">with Dmce</p></div>
-            <div class="input-group mb-4 ">
-  <input type="text" class="form-control" placeholder="Search your name" aria-label="Username" aria-describedby="basic-addon1" value={value} onChange={filterData}/>
-</div>
-        <table>
-            <thead class="thead">
-                <tr>
-                    <th class="thclass">Sr.No</th>
-                    <th class="thclass">Name</th>
-                    <th class="thclass">Course</th>
-                    <th class="thclass">skill</th>
-                    <th class="thclass">Gen Ai</th>
-                    <th class="thclass">Due Date</th>
-                    <th class="thclass">Status</th>
-                </tr>
+    <div className="table__body">
+      <img src={image1} id="img1" alt="Image 1" />
+      <img src={image2} id="img2" alt="Image 2" />
+      <img src={image3} id="img3" alt="Image 3" />
+      <img src={image4} id="img4" alt="Image 4" />
+
+      <div className="tableinfo">
+        <div className="Texthead">
+          <h1>Google Cloud Study Jams 23-24</h1>
+          <p className="Para">with Dmce</p>
+        </div>
+        <div className="input-group mb-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search your name"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            value={value}
+            onChange={filterData}
+          />
+        </div>
+        <div className="table__container">
+          <table>
+            <thead className="thead">
+              <tr>
+                <th>Sr.No</th>
+                <th>Name</th>
+                <th>Course</th>
+                <th>Skill</th>
+                <th>Gen Ai</th>
+                <th>Due Date</th>
+                <th>Status</th>
+              </tr>
             </thead>
             <tbody>
-                {value.length > 0 ? tableFilter.map((data) => {
-                    return(
-                        <tr>
-                            <td class="tdclass">{data.id}</td>
-                            <td class="tdclass">{data.NAME}</td>
-                            <td class="tdclass">{data.COURSES}</td>
-                            <td class="tdclass">{data.SKILL}</td>
-                            <td class="tdclass">{data.GEN_AI}</td>
-                            <td class="tdclass">{data.DUE_DATE}</td>
-                            <td class="tdclass">{data.STATUS}</td>
-                            <td class="tdclass"> <p className={data.STATUS}></p></td>
-                        </tr>
-                    )
-                })
-            : 
-            dataSource.map((data) => {
-                return(
-                    <tr>
-                        <td class="tdclass">{data.id}</td>
-                        <td class="tdclass">{data.NAME}</td>
-                        <td class="tdclass">{data.COURSES}</td>
-                        <td class="tdclass">{data.SKILL}</td>
-                        <td class="tdclass">{data.GEN_AI}</td>
-                        <td class="tdclass">{data.DUE_DATE}</td>
-                        <td class="tdclass">{data.STATUS}</td>
-                        <td class="tdclass"> <p className={data.STATUS}></p></td>
-                    </tr>
-                )
-            })
-            }
+              {value.length > 0
+                ? tableFilter.map((data) => {
+                    return (
+                      <tr key={data.id}>
+                        <td className="tdclass">{data.id}</td>
+                        <td className="tdclass">{data.NAME}</td>
+                        <td className="tdclass">{data.COURSES}</td>
+                        <td className="tdclass">{data.SKILL}</td>
+                        <td className="tdclass">{data.GEN_AI}</td>
+                        <td className="tdclass">{data.DUE_DATE}</td>
+                        <td className="tdclass">{data.STATUS}</td>
+                        <td className="tdclass">
+                          <p className={data.STATUS}></p>
+                        </td>
+                      </tr>
+                    );
+                  })
+                : dataSource.map((data) => {
+                    return (
+                      <tr key={data.id}>
+                        <td className="tdclass">{data.id}</td>
+                        <td className="tdclass">{data.NAME}</td>
+                        <td className="tdclass">{data.COURSES}</td>
+                        <td className="tdclass">{data.SKILL}</td>
+                        <td className="tdclass">{data.GEN_AI}</td>
+                        <td className="tdclass">{data.DUE_DATE}</td>
+                        <td className="tdclass">{data.STATUS}</td>
+                        <td className="tdclass">
+                          <p className={data.STATUS}></p>
+                        </td>
+                      </tr>
+                    );
+                  })}
             </tbody>
-        </table>
+          </table>
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Table;
-
-
-
-
